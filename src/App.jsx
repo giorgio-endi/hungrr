fc17c12729b13606002d1f028d60b8e072796156
hannahfsee <hannahfrance@student.unimelb.edu.au>
2026-03-13 20:37:04 +1100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>HUNGRR</h1>
-      <p>hello again</p>
-      <p>hackathon</p>
-      <p>Next step: Firebase connection</p>
-    </div>
+    <div style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h2>Screen 1</h2>
+      <div
+        style={{
+          border: "1px solid #ddd",
+          borderRadius: "20px",
+          padding: "20px",
+          marginBottom: "40px",
+          maxWidth: "700px",
+        }}
+      >
+        <p>HUNGRR</p>
+        <button style={{ display: "block", marginBottom: "10px" }}>
+          Create Room
+        </button>
+        <button>Join Room</button>
+      </div>
+
   );
 }
 
