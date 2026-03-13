b78056b0616fa0f716e097297e15af51a28d9b9e
Vince <vincentanter0509@gmail.com>
2026-03-13 20:15:14 +1100

    p

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{ padding: "40px" }}>
       <h1>HUNGRR</h1>
-      <p>hello!</p>
+      <p>boobie</p>
       <p>hackathon</p>
       <p>Next step: Firebase connection</p>
     </div>
