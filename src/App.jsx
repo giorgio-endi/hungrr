6a4350208361a22a20321bf148bac41c84074c25
Giorgio Endy <giorgioendy@a-2.local>
2026-03-13 20:12:16 +1100

    describe what you changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div style={{ padding: "40px" }}>
       <h1>HUNGRR</h1>
       <p>hello!</p>
+      <p>hackathon</p>
       <p>Next step: Firebase connection</p>
     </div>
   );
