1a40383de3e47c554cede95596cde0db54a6904b
Giorgio Endy <giorgioendy@a-2.local>
2026-03-14 21:27:36 +1100

    changed words

--- src/DecideHomeScreen.jsx
+++ src/DecideHomeScreen.jsx
@@ -22,7 +22,7 @@ function DecideHomeScreen({
         marginTop: "5px",
         marginBottom: "10px",
         boxSizing: "border-box",
-        color:"#000000",
+        color: "#000000",
         backgroundColor: "#fdd0cd"
     };
 
@@ -33,7 +33,7 @@ function DecideHomeScreen({
         borderRadius: "7px",
         border: "none",
         backgroundColor: hovered === name ? "#781715" : "#991c1a",
-        fontFamily:"Ubuntu",
+        fontFamily: "Ubuntu",
         color: "white",
         cursor: "pointer",
         marginTop: "18px",
@@ -43,89 +43,89 @@ function DecideHomeScreen({
     });
 
     const formBoxStyle = {
-    display: "flex",
-    flexDirection: "column",
-    borderRadius: "18px",
-    padding: "0px",
-    width: "100%",
-    maxWidth: "260px",
-    margin: "17px auto",
-    textAlign: "left",
+        display: "flex",
+        flexDirection: "column",
+        borderRadius: "18px",
+        padding: "0px",
+        width: "100%",
+        maxWidth: "260px",
+        margin: "17px auto",
+        textAlign: "left",
     };
 
     const labelStyle = {
-    fontSize: "13px",
-    fontWeight: "600",
-    color: "#020100",
-    marginTop: "10px",
+        fontSize: "13px",
+        fontWeight: "600",
+        color: "#020100",
+        marginTop: "10px",
     };
 
     return (
         <>
-        <h1>
-            Taste & Decide
-        </h1>
+            <h1>
+                Find Your Restaurant!
+            </h1>
 
-        <div style={formBoxStyle}>
+            <div style={formBoxStyle}>
 
-            <p style={labelStyle}>Name</p>
-            <input
-                type="text"
-                placeholder="Enter your name"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                style={inputStyle}
-            />
+                <p style={labelStyle}>Name</p>
+                <input
+                    type="text"
+                    placeholder="Enter your name"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    style={inputStyle}
+                />
 
-            <p style={labelStyle}>Location</p>
-            <input
-                type="text"
-                placeholder="Enter location (for host)"
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-                style={inputStyle}
-            />
+                <p style={labelStyle}>Location</p>
+                <input
+                    type="text"
+                    placeholder="Enter location (for host)"
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                    style={inputStyle}
+                />
 
-            <p style={labelStyle}>Room code</p>
-            <input
-                type="text"
-                placeholder="Enter room code (for guest)"
-                value={joinCode}
-                onChange={(e) => setJoinCode(e.target.value.toUpperCase())}
-                style={inputStyle}
-            />
+                <p style={labelStyle}>Room code</p>
+                <input
+                    type="text"
+                    placeholder="Enter room code (for guest)"
+                    value={joinCode}
+                    onChange={(e) => setJoinCode(e.target.value.toUpperCase())}
+                    style={inputStyle}
+                />
 
-        </div>
+            </div>
 
-         <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                gap: "1px", 
-                marginTop: "0px", 
-                padding: "0px", 
-                borderRadius: "16px",
-            }}
-        >
-            <button
-                onClick={onCreateRoom}
-                onMouseEnter={() => setHovered("create")}
-                onMouseLeave={() => setHovered(null)}
-                style={mainButtonStyle("create")}
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: "1px",
+                    marginTop: "0px",
+                    padding: "0px",
+                    borderRadius: "16px",
+                }}
             >
-                Create Room
-            </button>
+                <button
+                    onClick={onCreateRoom}
+                    onMouseEnter={() => setHovered("create")}
+                    onMouseLeave={() => setHovered(null)}
+                    style={mainButtonStyle("create")}
+                >
+                    Create Room
+                </button>
 
-            <button
-                onClick={onJoinRoom}
-                onMouseEnter={() => setHovered("join")}
-                onMouseLeave={() => setHovered(null)}
-                style={mainButtonStyle("join")}
-            >
-                Join Room
-            </button>
-        </div>
+                <button
+                    onClick={onJoinRoom}
+                    onMouseEnter={() => setHovered("join")}
+                    onMouseLeave={() => setHovered(null)}
+                    style={mainButtonStyle("join")}
+                >
+                    Join Room
+                </button>
+            </div>
 
             {roomCode !== "" && decideScreen === "home" && (
                 <p style={{ marginTop: "20px", fontSize: "18px", color: "#1f5f8b" }}>
